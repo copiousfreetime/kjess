dd5d709561abdb6498051a320f3f1c79aead40a9
Jeremy Hinegardner <jeremy@hinegardner.org>
2012-10-28 22:56:46 -0600

    fix spec with right parameter

--- spec/request/set_spec.rb
+++ spec/request/set_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe KJess::Request::Set do
   it "converts to the protocol" do
-    s = KJess::Request::Set.new( :queue => 'test', :data => 'a job' )
+    s = KJess::Request::Set.new( :queue_name => 'test', :data => 'a job' )
     s.to_protocol.must_equal "SET test 0 0 5\r\na job\r\n"
   end
 
   it "sets the expriation time" do
-    s = KJess::Request::Set.new( :queue => 'test', :expiration => 42, :data => 'a job' )
+    s = KJess::Request::Set.new( :queue_name => 'test', :expiration => 42, :data => 'a job' )
     s.to_protocol.must_equal "SET test 0 42 5\r\na job\r\n"
   end
 end
