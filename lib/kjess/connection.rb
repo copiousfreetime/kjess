6f85496fb251517009f3c7dd9e7a9afcc2d7fb9e
Jeremy Hinegardner <jeremy@hinegardner.org>
2013-01-08 20:38:56 -0500

    Extract out KJess::Socket from KJess::Connection

--- lib/kjess/connection.rb
+++ lib/kjess/connection.rb
@@ -1,75 +1,59 @@
 require 'fcntl'
-require 'socket'
 require 'resolv'
 require 'resolv-replace'
 require 'kjess/error'
+require 'kjess/socket'
 
 module KJess
   # Connection
   class Connection
     class Error < KJess::Error; end
-    class Timeout < Error; end
 
-    # Public:
-    # The hostname/ip address to connect to
-    attr_reader :host
+    # Public: The hostname/ip address to connect to.
+    def host
+      socket.host
+    end
 
-    # Public
-    # The port number to connect to. Default 22133
-    attr_reader :port
+    # Public: The port number to connect to. Default 22133
+    def port
+      socket.port
+    end
 
-    # Public
-    # The timeout for connecting in seconds. Defaults to 2
-    attr_accessor :connect_timeout
+    # Public: The timeout for connecting in seconds. Defaults to 2
+    def connect_timeout
+      socket.connect_timeout
+    end
 
-    # Public
-    # The timeout for reading in seconds. Defaults to 2
-    attr_accessor :read_timeout
+    # Public: The timeout for reading in seconds. Defaults to 2
+    def read_timeout
+      socket.read_timeout
+    end
 
-    # Public
-    # The timeout for writing in seconds. Defaults to 2
-    attr_accessor :write_timeout
+    # Public: The timeout for writing in seconds. Defaults to 2
+    def write_timeout
+      socket.write_timeout
+    end
 
-    # Internal
-    #
-    # Used for setting TCP_KEEPIDLE: overrides tcp_keepalive_time for a single
-    # socket.
-    #
-    # http://tldp.org/HOWTO/TCP-Keepalive-HOWTO/usingkeepalive.html
-    #
-    # tcp_keepalive_time:
-    #
-    #  The interval between the last data packet sent (simple ACKs are not
-    #  considered data) and the first keepalive probe; after the connection is
-    #  marked to need keepalive, this counter is not used any further.
-    attr_reader :keepalive_idle
+    # Internal: return thekeepalive timeout
+    def keepalive_active?
+      socket.keepalive_active?
+    end
 
-    # Internal
-    #
-    # Used for setting TCP_KEEPINTVL: overrides tcp_keepalive_intvl for a single
-    # socket.
-    #
-    # http://tldp.org/HOWTO/TCP-Keepalive-HOWTO/usingkeepalive.html
-    #
-    # tcp_keepalive_intvl:
-    #
-    #   The interval between subsequential keepalive probes, regardless of what
-    #   the connection has exchanged in the meantime.
-    attr_reader :keepalive_interval
+    # Internal: return the keepalive count
+    # The keepalive count
+    def keepalive_count
+      socket.keepalive_count
+    end
 
-    # Internal
-    #
-    # Used for setting TCP_KEEPCNT: overrides tcp_keepalive_probes for a single
-    # socket.
-    #
-    # http://tldp.org/HOWTO/TCP-Keepalive-HOWTO/usingkeepalive.html
-    #
-    # tcp_keepalive_probes:
-    #
-    #   The number of unacknowledged probes to send before considering the
-    #   connection dead and notifying the application layer.
-    attr_reader :keepalive_count
+    # Internal: return the keepalive interval
+    def keepalive_interval
+      socket.keepalive_interval
+    end
 
+    # Internal: return the keepalive idle
+    def keepalive_idle
+      socket.keepalive_idle
+    end
 
     # TODO: make port an option at next major version number change
     def initialize( host, port = 22133, options = {} )
@@ -78,18 +62,9 @@ module KJess
         port = 22133
       end
 
-      @host            = host
-      @port            = Float( port ).to_i
-
-      @connect_timeout = options.fetch(:connect_timeout, 2)
-      @read_timeout    = options.fetch(:read_timeout   , 2)
-      @write_timeout   = options.fetch(:write_timeout  , 2)
-
-      @keepalive_active   = options.fetch(:keepalive_active, true)
-      @keepalive_idle     = options.fetch(:keepalive_idle    , 60)
-      @keepalive_interval = options.fetch(:keepalive_interval, 30)
-      @keepalive_count    = options.fetch(:keepalive_count,    5)
-
+      @options         = options.dup
+      @options[:host] = host
+      @options[:port] = Float( port ).to_i
       @socket          = nil
       @pid             = nil
       @read_buffer     = ''
@@ -101,119 +76,28 @@ module KJess
     #
     # Returns nothing
     def with_additional_read_timeout(additional_timeout, &block)
-      old_read_timeout, @read_timeout = @read_timeout, @read_timeout + additional_timeout
+      old_read_timeout, @read_timeout = read_timeout, read_timeout + additional_timeout
       block.call
     ensure
       @read_timeout = old_read_timeout
     end
 
-    # Internal: Return the raw socket that is connected to the Kestrel server
+    # Internal: Return the socket that is connected to the Kestrel server
     #
-    # Returns the raw socket. If the socket is not connected it will connect and
+    # Returns the socket. If the socket is not connected it will connect and
     # then return it.
     #
     # Make sure that we close the socket if we are not the same process that
     # opened that socket to begin with.
     #
-    # Returns a TCPSocket
+    # Returns a KJess::Socket
     def socket
       close if @pid && @pid != Process.pid
       return @socket if @socket and not @socket.closed?
-      @socket      = connect()
+      @socket      = Socket.connect( @options )
       @pid         = Process.pid
       @read_buffer = ''
       return @socket
-    end
-
-    # Internal: Low level socket allocation and option configuration
-    #
-    # Returns a new Socket instance
-    def blank_socket
-      sock = ::Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
-
-      # close file descriptors if we exec
-      sock.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
-
-      # Disable Nagle's algorithm
-      sock.setsockopt(::Socket::IPPROTO_TCP, ::Socket::TCP_NODELAY, 1)
-
-      if using_keepalive? then
-        sock.setsockopt( Socket::SOL_SOCKET, Socket::SO_KEEPALIVE , true )
-        sock.setsockopt( Socket::SOL_TCP,    Socket::TCP_KEEPIDLE , keepalive_idle )
-        sock.setsockopt( Socket::SOL_TCP,    Socket::TCP_KEEPINTVL, keepalive_interval)
-        sock.setsockopt( Socket::SOL_TCP,    Socket::TCP_KEEPCNT  , keepalive_count)
-      end
-
-      return sock
-
-    end
-
-    # Internal: say if we are using TCP Keep Alive or not
-    #
-    # We will return true if the initialization options :keepalive_active is
-    # set to true, and if all the constants that are necessary to use TCP keep
-    # alive are defined.
-    #
-    # It may be the case that on some operating systems that the constants are
-    # not defined, so in that case we do not want to attempt to use tcp keep
-    # alive if we are unable to do so in any case.
-    #
-    # Returns true or false
-    def using_keepalive?
-      using = false
-      if @keepalive_active then
-        using = [ :SOL_SOCKET, :SO_KEEPALIVE, :SOL_TCP, :TCP_KEEPIDLE, :TCP_KEEPINTVL, :TCP_KEEPCNT].all? do |c|
-          Socket.const_defined? c
-        end
-      end
-      return using
-    end
-    # Internal: Create the socket we use to talk to the Kestrel server
-    #
-    # Returns a Socket
-    def connect
-      exception = nil
-
-      # Calculate our timeout deadline
-      deadline = Time.now.to_f + @connect_timeout
-
-      # Lookup address, we only want           IPv4 , TCP
-      addrs = ::Socket.getaddrinfo(host, port, ::Socket::AF_INET, ::Socket::SOCK_STREAM )
-
-      addrs.each do |addr|
-        timeout = deadline - Time.now.to_f
-        raise Timeout, "Could not connect to #{host}:#{port}" unless timeout > 0
-        sockaddr = ::Socket.pack_sockaddr_in(addr[1], addr[3])
-        sock     = blank_socket()
-
-        begin
-          begin
-            sock.connect_nonblock( sockaddr )
-          rescue Errno::EINPROGRESS
-            if IO.select(nil, [sock], nil, timeout) == nil
-              raise Timeout, "Could not connect to #{host}:#{port}"
-            end
-
-            begin
-              sock.connect_nonblock( sockaddr )
-            rescue Errno::EISCONN
-            rescue => ex
-              exception = ex
-              next
-            end
-          rescue => ex
-            exception = ex
-            next
-          end
-
-          return sock
-        rescue
-          sock.close
-          raise
-        end
-      end
-
-      raise Error, "Could not connect to #{host}:#{port}: #{exception.class}: #{exception.message}", exception.backtrace
     end
 
     # Internal: close the socket if it is not already closed
@@ -241,20 +125,8 @@ module KJess
     # Returns nothing
     def write( msg )
       $stderr.puts "--> #{msg}" if $DEBUG
-
-      begin
-        until msg.length == 0
-          written = socket.write_nonblock(msg)
-          msg = msg[written, msg.length]
-        end
-      rescue Errno::EWOULDBLOCK, Errno::EINTR, Errno::EAGAIN, Errno::ECONNRESET
-        if IO.select(nil, [socket], nil, @write_timeout)
-          retry
-        else
-          raise Timeout, "Could not write to #{host}:#{port} in #{@write_timeout} seconds"
-        end
-      end
-    rescue Timeout
+      socket.write( msg )
+    rescue KJess::Error
       close
       raise
     end
@@ -267,16 +139,15 @@ module KJess
     def readline( eom = Protocol::CRLF )
       while true
         while (idx = @read_buffer.index(eom)) == nil
-          @read_buffer << readpartial(10240)
+          @read_buffer << socket.readpartial(10240)
         end
 
         line = @read_buffer.slice!(0, idx + eom.length)
         $stderr.puts "<-- #{line}" if $DEBUG
         break unless line.strip.length == 0
       end
-
       return line
-    rescue Timeout
+    rescue KJess::Error
       close
       raise
     rescue EOFError
@@ -294,26 +165,16 @@ module KJess
     # Returns what IO#read returns
     def read( nbytes )
       while @read_buffer.length < nbytes
-        @read_buffer << readpartial(nbytes - @read_buffer.length)
+        @read_buffer << socket.readpartial(nbytes - @read_buffer.length)
       end
 
       result = @read_buffer.slice!(0, nbytes)
 
       $stderr.puts "<-- #{result}" if $DEBUG
       return result
-    rescue Timeout
+    rescue KJess::Error
       close
       raise
-    end
-
-    def readpartial(maxlen, outbuf = nil)
-      return socket.read_nonblock(maxlen, outbuf)
-    rescue Errno::EWOULDBLOCK, Errno::EAGAIN, Errno::ECONNRESET
-      if IO.select([socket], nil, nil, @read_timeout)
-        retry
-      else
-        raise Timeout, "Could not read from #{host}:#{port} in #{@read_timeout} seconds"
-      end
     end
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -292,14 +292,14 @@ describe KJess::Client do
   describe "connecting to a server on a port that isn't listening" do
     it "throws an exception" do
       c = KJess::Connection.new '127.0.0.1', 65521
-      lambda { c.socket }.must_raise KJess::Connection::Error
+      lambda { c.socket }.must_raise KJess::Socket::Error
     end
   end
 
   describe "connecting to a server that isn't responding" do
     it "throws an exception" do
       c = KJess::Connection.new '127.1.1.1', 65521, :timeout => 0.5
-      lambda { c.socket }.must_raise KJess::Connection::Timeout
+      lambda { c.socket }.must_raise KJess::Socket::Timeout
     end
   end
 
@@ -321,7 +321,7 @@ describe KJess::Client do
       q.deq
       c = KJess::Connection.new '127.0.0.1', 65520, :timeout => 0.5
 
-      lambda { c.readline }.must_raise KJess::Connection::Timeout
+      lambda { c.readline }.must_raise KJess::Socket::Timeout
 
       t.run
       t.join
@@ -345,7 +345,7 @@ describe KJess::Client do
       q.deq
       c = KJess::Connection.new '127.0.0.1', 65520, :timeout => 0.5
 
-      lambda { c.write('a' * 10000000) }.must_raise KJess::Connection::Timeout
+      lambda { c.write('a' * 10000000) }.must_raise KJess::Socket::Timeout
 
       t.run
       t.join
